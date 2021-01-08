4feba220be5edeec58f82a27a6941a0b56ae228f
asymmenguin <tibicen1@gmail.com>
2021-01-07 22:00:14 -0700

    created class like object structure

---
+++ tools/ModificationKey.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import random
+from Crypto.Util import number
+from Crypto.PublicKey import RSA
+
+class ModificationKey():
+    def __init__(self):
+        self.name = 'tavian'
+
+    def pri(self):
+        print (self.name)
