bdd0423c47cb9b359160c959298023005bbee82f
Carter Fulcher <carter@fulchereng.com>
2021-05-03 11:19:49 -0600

    update README, move createKey to utils

--- quarkz/rsa.py
+++ quarkz/rsa.py
@@ -2,68 +2,23 @@ import random
 import sys
 import json
 import decimal
+
 from base64 import b64encode, b64decode
+from decimal import Decimal
+
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util import number
 from Crypto.PublicKey import RSA 
-from decimal import Decimal
+
 from quarkz import utils 
-from quarkz.dtypes import Encrypted, KeyPair
+from quarkz.dtypes import Encrypted 
 import quarkz
 
 decimal.getcontext().prec=100000
 
-def createKey(keySize: int):
-    p = number.getPrime(keySize)
-    q = number.getPrime(keySize)
-    n = Decimal(p*q)
-    phi = Decimal((p-1)*(q-1))
-    while True:
-        e = Decimal(number.getPrime(10))
-        r = utils.gcd(int(e), int(phi))
-        if r == 1:
-            break
-    while True:
-        j = random.randint(1000, 10000)
-        o = (e-1)**j
-        check = utils.gcd(int(e), int(o-1))
-        if check != 1:
-            break
 
-    d = Decimal(utils.mod_inverse(int(e), int(phi)))
-    
-    diff = abs(n-Decimal(o))
-
-
-    if diff > 0:
-        ratio = n/diff
-    else:
-        u = random.randint(0, 1000)
-        o -= u
-        diff = abs(n-o) % n
-        ratio = n/diff
-
-    privateKey = {
-        "d": d,
-        "n": n,
-        "diff": diff,
-    }
-
-    publicKey = {
-        "e": e,
-        "o": o,
-        "ratio": ratio
-    }
-
-    keyPair = {
-        "private_key": privateKey,
-        "public_key": publicKey
-    }
-
-    return KeyPair(**keyPair)
-
-def encrypt(message: int, publicKey: dict) -> tuple: 
+def encrypt(message: int, publicKey: dict) -> quarkz.dtypes.Encrypted: 
 
     assert(type(message) == int)
 
@@ -102,7 +57,7 @@ def decrypt(encrypted: quarkz.dtypes.Encrypted, keypair: quarkz.dtypes.KeyPair) 
 
 if __name__ == "__main__":
     #first, create a new key pair 
-    pair = createKey(1024)
+    pair = utils.createKey(1024)
 
     #encrypt some data
     message = 69
--- quarkz/utils.py
+++ quarkz/utils.py
@@ -1,3 +1,7 @@
+from Crypto.Util import number
+from decimal import Decimal
+from quarkz.dtypes import KeyPair
+import random
 
 def gcd(a, b): 
    while a != 0:
@@ -25,3 +29,52 @@ def modpow(x,n,m):
     return modpow(x*(x%m),n/2,m)%m
   elif n%2 == 1:
     return (x *  modpow(x*(x%m),(n-1)/2,m)%m )%m
+
+def createKey(keysize: int = 1024):
+    p = number.getPrime(keysize)
+    q = number.getPrime(keysize)
+    n = Decimal(p*q)
+    phi = Decimal((p-1)*(q-1))
+    while True:
+        e = Decimal(number.getPrime(10))
+        r = gcd(int(e), int(phi))
+        if r == 1:
+            break
+    while True:
+        j = random.randint(1000, 10000)
+        o = (e-1)**j
+        check = gcd(int(e), int(o-1))
+        if check != 1:
+            break
+
+    d = Decimal(mod_inverse(int(e), int(phi)))
+    
+    diff = abs(n-Decimal(o))
+
+
+    if diff > 0:
+        ratio = n/diff
+    else:
+        u = random.randint(0, 1000)
+        o -= u
+        diff = abs(n-o) % n
+        ratio = n/diff
+
+    privateKey = {
+        "d": d,
+        "n": n,
+        "diff": diff,
+    }
+
+    publicKey = {
+        "e": e,
+        "o": o,
+        "ratio": ratio
+    }
+
+    keyPair = {
+        "private_key": privateKey,
+        "public_key": publicKey
+    }
+
+    return KeyPair(**keyPair)
